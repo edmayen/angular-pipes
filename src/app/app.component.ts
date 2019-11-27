9737dfa4155494d66f93a3a94dacfdc1e1fee658
Eduardo Mayen <eduardo.mayen@ies-systems.com>
2019-11-27 14:26:38 -0600

    version 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { resolve } from 'url';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes';
+
+  name = "Eduardo"
+  name2 = "eduardo mayen PanTaleOn"
+
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a: number = 0.234;
+
+  salary: number = 1234.5;
+
+  hero = {
+    name: "Bruce",
+    alias: "Batman",
+    age: 45,
+    jl: {
+      flash: "Barry Allen",
+      greenArrow: "Oliver Queen",
+      wonderWoman: "Diana Prince"
+    }
+  }
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout( () => resolve('Llego la data!'), 3500);
+  })
+
+  date = new Date();
+
+  video = 'UUyJoojCLnc';
+
+  activar: boolean = true;
 }
